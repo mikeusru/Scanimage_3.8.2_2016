a2a974c161d1984e7ed4c8f799d437236a7b877a
yasudalab <yasudalab@mpfi.org>
2018-04-06 14:14:23 -0400

    First Commit

---
+++ Uncaging Automation/takeStackOfEntireFOV.m
@@ -0,0 +1,23 @@
+function takeStackOfEntireFOV
+global state gh dia
+ds = dia.hPos.allPositionsDS;
+zRange = [min(ds.motorZ) - (state.acq.numberOfZSlices*state.acq.zStepSize/2),...
+    max(ds.motorZ) + (state.acq.numberOfZSlices*state.acq.zStepSize/2)];
+
+%set Z center
+zCenter = min(ds.motorZ) + range(ds.motorZ)/2;
+
+%set appropriate slice number
+numZSlices = ceil(range(zRange)/state.acq.zStepSize);
+
+%set large scam pixel count
+pixelsPerLine = 1024;
+linesPerFrame = 1024;
+valref = strfind(get(gh.configurationControls.pixelsPerLine,'String'),num2str(pixelsPerLine));
+val = find(~cellfun(@isempty,valref));
+set(gh.configurationControls.pixelsPerLine, 'Value', val);
+state.acq.pixelsPerLine = pixelsPerLine;
+genericCallback(gh.configurationControls.pixelsPerLine);
+state.acq.linesPerFrame = linesPerFrame;
+set(gh.configurationControls.linesPerFrame, 'String', num2str(linesPerFrame));
+genericCallback(gh.configurationControls.linesPerFrame);
